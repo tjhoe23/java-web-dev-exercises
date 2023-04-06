e006bc78e876bb8e964b85bd7dd6345c3e18d4b8
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-04-06 14:12:57 -0500

    Studio Ch 3 4/3

---
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private Date lastUpdated;
+
+}
