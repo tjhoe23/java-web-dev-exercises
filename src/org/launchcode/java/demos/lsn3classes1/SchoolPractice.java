d4537585e31138a299f4fd1cf6c1b384d5648639
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-04-06 13:56:20 -0500

    Exercise and Studio 3/30

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -4,4 +4,11 @@ public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
     }
+
+    public class Course {
+        private String topic;
+        private Teacher instructor;
+
+        private ArrayList<student> enrolledStudents;
+    }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -7,7 +7,40 @@ public class Student {
 
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits = 1;
+    private double gpa = 4.0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
 
 }
