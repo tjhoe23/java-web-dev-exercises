4ee71a77c6d9724dc22c1c6d80ec7c57df34a436
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-04-17 18:13:23 -0500

    Ch 8 Interfaces and Poly Exercise Fixed

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -8,17 +8,21 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
-        Comparator comparator = new FlavorComparator();
 
-        flavors.sort(comparator);
+        Comparator flavorComparator = new FlavorComparator();
+        Comparator coneComparator = new ConeComparator();
+        flavors.sort(flavorComparator);
+        cones.sort(coneComparator);
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+        System.out.println("Flavors:");
+        for(Flavor flavor : flavors) {
+            System.out.println(flavor.getName());
+        }
+        System.out.println("------------------");
+        System.out.println("Cones:");
+        for(Cone cone : cones) {
+            System.out.println(cone.getName() + " $" + cone.getCost());
+        }
 
-
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
-
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
 }
